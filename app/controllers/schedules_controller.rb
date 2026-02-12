323f8970792c6a936c5e48e6d580147382d0c78f
remo0619 <ichinohe@aomori-innovators.com>
2026-02-12 22:24:15 +0900

    Read・Update・Delete実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -18,14 +18,27 @@ class SchedulesController < ApplicationController
   end
 
   def show
+    @schedule = Schedule.find(params[:id])
   end
 
   def edit
+    @schedule = Schedule.find(params[:id])
   end
 
   def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(params.require(:schedule).permit(:title, :start_date, :end_date, :schedule_memo))
+      flash[:notice] = "IDが「#{@schedule.id}」の予定を更新しました"
+      redirect_to :schedules
+    else
+      render "edit", status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    flash[:notice] = "「#{@schedule.title}」を削除しました"
+    redirect_to :schedules
   end
 end
