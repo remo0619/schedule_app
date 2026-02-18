f45c46979629ae5ed1d3dce02732c06de1bcc457
remo0619 <ichinohe@aomori-innovators.com>
2026-02-18 17:17:34 +0900

    bootstrap-material-datetimepicker実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"
+// import "data-confirm-modal"
+
+$(document).on('turbo:load', function() {
+  console.log("jQuery check:", typeof $); // コンソールで確認用
+
+  if (typeof $.fn.bootstrapMaterialDatePicker !== 'undefined') {
+    $('.datetimepicker').bootstrapMaterialDatePicker({
+      format: 'YYYY-MM-DD',
+      lang: 'ja',
+      weekStart: 0, 
+      cancelText: 'キャンセル',
+      okText: '決定',
+      time: false,
+      date: true
+    });
+  } else {
+    console.error("Datetimepicker plugin not found!");
+  }
+});
